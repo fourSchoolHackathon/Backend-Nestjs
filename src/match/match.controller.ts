3ee2b5df8b50b38e1352f48a2fca9f62d9a8a3e4
leehj050211 <leehj050211@gmail.com>
2022-10-06 19:50:01 +0900

    FIX: 매칭 완료 메시지 보내기 API HTTP 코드 변경

--- src/match/match.controller.ts
+++ src/match/match.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode } from '@nestjs/common';
 import { MatchSuccessRequestDto } from 'src/match/dto/request/matchSuccessRequest.dto';
 import { MatchService } from 'src/match/match.service';
 
@@ -6,6 +6,7 @@ import { MatchService } from 'src/match/match.service';
 export class MatchController {
     constructor(private readonly matchService: MatchService) {}
 
+    @HttpCode(200)
     @Post()
     match(
         @Body() dto: MatchSuccessRequestDto
